b2ea340feb037d0c4e6eb06fd22100e1f86743ee
FrontMD <web@mockup.digital>
2024-10-23 17:03:36 +0300

    table show more in progress

--- src/blocks/modules/m-table/m-table.js
+++ src/blocks/modules/m-table/m-table.js
@@ -1,8 +1,20 @@
 function mTable() {
-    const linesCount = 6 // сколько строк оставлять в ячейке 
+    const linesCount = 5 // сколько строк оставлять в ячейке 
     const tables = document.querySelectorAll('[data-js="mTable"]')
 
     if(tables.length < 1) return
+
+    const showMoreBtnLayout = `
+                                <span class="show-more">
+                                    <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M13.8112 6.1088V9.2192H8.7136V14.7488H5.2576V9.2192H0.16V6.1088H5.2576V0.579199H8.7136V6.1088H13.8112Z" fill="#015738"/>
+                                    </svg>
+                                    показать все
+                                <span>
+                                <span class="show-less">
+                                    скрыть
+                                <span>
+                            `
 
     tables.forEach(table => {
         const cells = table.querySelectorAll('td')
@@ -12,15 +24,27 @@ function mTable() {
             let cellLineHeight = parseFloat(styles["line-height"].replace(/[a-zA-Z]/g, ""))
             let cellContent = cell.innerHTML
             let cellContentWrap = document.createElement('div')
-
+            let compressedСontentHeight = cellLineHeight * linesCount
+            
             cellContentWrap.innerHTML = cellContent
             cell.innerHTML = ''
             cell.appendChild(cellContentWrap)
 
-            if(cellContentWrap.offsetHeight > cellLineHeight * linesCount) {
-                console.log(cellContentWrap.offsetHeight)
-                console.log(cellLineHeight * 5)
+            if(cellContentWrap.offsetHeight > compressedСontentHeight) {
+                cellContentWrap.classList.add('m-table__cell-content')
+                cellContentWrap.setAttribute('data-height', cellContentWrap.offsetHeight)
+                cellContentWrap.style.maxHeight = compressedСontentHeight + 'px'
+                
+                cell.classList.add('collapsed')
+
+                
+                let showMoreBtn = document.createElement('button')
+                showMoreBtn.classList.add("m-table__showmore")
+                showMoreBtn.innerHTML = showMoreBtnLayout
                 console.log(cell)
+                console.log(showMoreBtn)
+
+                cell.appendChild(showMoreBtn)
             }
             
         })
