cf5030c1251cc20eca5d6d14522fe97f41ab5ebd
FrontMD <web@mockup.digital>
2024-11-02 16:00:04 +0300

    titles fix

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,13 @@
 @@include("./libs/air-datepicker.js")
 @@include("./libs/fancybox.umd.min.js")
 @@include("./libs/swiper-bundle.min.js")
+@@include("./libs/inputmask.min.js")
 
 //если желаете подключить библиотеку - раскомментируйте и добавьте @@
 // include("./libs/nouislider.min.js")
 // include("./libs/wNumb.min.js")
 // include("./libs/select2.full.min.js")
 // include("./libs/autosize.min.js")
-// include("./libs/inputmask.min.js")
 // include("./libs/debounce.js")
 // include("./libs/simplebar.min.js")
 // include("./libs/js.cookie.min.js")
